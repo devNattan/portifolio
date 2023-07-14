52fcb16e04cc427603585c5accf0587ea755f019
dev_natte <natanaelhenrique.s.c@gmail.com>
2023-07-14 12:20:38 -0300

    ✅ adicionando a seção de intro

--- minimalist-portifolio/src/components/Intro.jsx
+++ minimalist-portifolio/src/components/Intro.jsx
@@ -2,7 +2,23 @@ import React from "react";
 
 function Intro() {
   return (
-    <div>Intro</div>
+    <div className="flex items-center 
+      justify-center flex-col 
+      text-center pt-20 pb-6">
+      
+      <h1 className="text-4xl md:text-7xl
+        mb-1 md:mb-3 font-bold">
+          Dev Nate
+      </h1>
+      <p className="text-base 
+        md:text-xl mb-3 font-medium">
+          Software Engineer & Web Developer
+      </p>
+      <p className="text-sm 
+        max-w-xl mb-6 font-bold">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores facere debitis dicta. Blanditiis facilis quibusdam molestias fugiat dolorem, similique odit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores sed, voluptatibus inventore, consequatur perferendis magni reprehenderit molestias minima ex explicabo rem minus. Illo iure odio amet, temporibus tenetur fugiat voluptas reiciendis deleniti inventore libero consequuntur? Inventore rerum nisi et sit.
+      </p>
+    </div>
   )
 }
 
