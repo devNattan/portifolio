17875fb9df1e47ac5b3027226b025e00202b6446
dev_natte <natanaelhenrique.s.c@gmail.com>
2023-07-14 16:15:59 -0300

    💄 estilizando os itens da timeline

--- minimalist-portifolio/src/components/Timeline.jsx
+++ minimalist-portifolio/src/components/Timeline.jsx
@@ -1,8 +1,21 @@
 import React from "react";
+import timeline from "../data/timeline";
+import TimelineItems from "./TimelineItems";
 
 function Timeline() {
   return (
-    <div>Timeline</div>
+    <div className="flex flex-col md:flex-row justify-center my-20 w-full">
+    <div className="w-full md:w-7/12">
+     {timeline.map(item => (
+      <TimelineItems 
+        year={item.year}
+        title={item.title}
+        duration={item.duration}
+        details={item.details}
+      />
+     ))}
+    </div>
+    </div>
   )
 }
 
--- minimalist-portifolio/src/data/timeline.js
+++ minimalist-portifolio/src/data/timeline.js
@@ -19,5 +19,26 @@ export default [
     duration: '6 months',
     details: 
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer at mauris imperdiet, finibus mauris in, convallis quam. Nulla vel porttitor velit, a elementum nisl. Sed ut lorem semper, tempor diam sit amet, fringilla mi. Proin ornare, turpis a tempus molestie, neque eros vulputate nisi, quis gravida lectus lacus."
+  },
+   {
+    year: 'Today',
+    title: 'Software Engineer & Web Developer',
+    duration: '6 months',
+    details: 
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer at mauris imperdiet, finibus mauris in, convallis quam. Nulla vel porttitor velit, a elementum nisl. Sed ut lorem semper, tempor diam sit amet, fringilla mi. Proin ornare, turpis a tempus molestie, neque eros vulputate nisi, quis gravida lectus lacus."
+  },
+   {
+    year: 'Today',
+    title: 'Software Engineer & Web Developer',
+    duration: '6 months',
+    details: 
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer at mauris imperdiet, finibus mauris in, convallis quam. Nulla vel porttitor velit, a elementum nisl. Sed ut lorem semper, tempor diam sit amet, fringilla mi. Proin ornare, turpis a tempus molestie, neque eros vulputate nisi, quis gravida lectus lacus."
+  },
+   {
+    year: 'Today',
+    title: 'Software Engineer & Web Developer',
+    duration: '6 months',
+    details: 
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer at mauris imperdiet, finibus mauris in, convallis quam. Nulla vel porttitor velit, a elementum nisl. Sed ut lorem semper, tempor diam sit amet, fringilla mi. Proin ornare, turpis a tempus molestie, neque eros vulputate nisi, quis gravida lectus lacus."
   }
 ]
